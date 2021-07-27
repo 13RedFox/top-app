e6968bba410d2de9d74b91629cfb6984687e9c79
Serhii Shabala <sergei181989@gmail.com>
2021-07-27 23:04:18 +0300

    :sparkles:Add Document template

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProp = await Document.getInitialProps(ctx);
+
+    return { ...initialProp };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
